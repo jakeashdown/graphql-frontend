aca34ffd116420a253a58734111d0b4846145778
jake ashdown <jake.ashdown@protonmail.com>
2020-04-05 13:43:15 +0200

    Adds Apollo client using local GraphQL endpoint
    Replace the app component with a simple echo query to the endpoint
    at http://localhost:8080/graphql, displaying the echoed value.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Apollo} from 'apollo-angular';
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  echoed: string;
+  loading = true;
+  error: any;
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.apollo
+      .watchQuery({
+        query: gql`
+          {
+            echoed: echo(toEcho: "data to echo")
+          }
+        `,
+      })
+      .valueChanges.subscribe(result => {
+        const data: any = result.data
+        this.echoed = data && data.echoed;
+
+        this.loading = result.loading;
+        
+        this.error = (result as any).error;
+      });
+  }
 }
