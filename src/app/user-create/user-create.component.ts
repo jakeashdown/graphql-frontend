e971d43708c5dc2f805a50927d44f05c9fe552bd
jake ashdown <jake.ashdown@protonmail.com>
2020-04-13 07:33:44 +0200

    Adds navigation button to create-user component

--- src/app/user-create/user-create.component.ts
+++ src/app/user-create/user-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-create',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserCreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  toUserList() {
+    this.router.navigate(['/users']);
   }
 
 }
